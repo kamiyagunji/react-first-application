744abe524039d38bde1c22201fe95ccee8788235
kamiyagunji <kamiyagunji@icloud.com>
2020-02-29 19:18:38 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function App() {
-  return <h1>Hello, world!</h1>;
+  return (
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={ () => {console.log("I am clieked!")}} />
+    </React.Fragment>
+  )
+
+  // return <h1>Hello, world!</h1>;
 }
 
 export default App;
