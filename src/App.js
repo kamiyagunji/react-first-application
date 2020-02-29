c0679d6f30dd3ffa7d25243002b1828edd4264b2
kamiyagunji <kamiyagunji@icloud.com>
2020-02-29 16:49:01 +0900

    Deleted p

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Hello, world!</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
